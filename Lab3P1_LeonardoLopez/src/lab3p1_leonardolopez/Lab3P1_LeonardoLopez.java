b0adce0c4da9b7592d0df7a4447234f407a58140
Leo0w0 <leonardo.lopez001@outlook.com>
2024-10-25 14:07:16 -0600

    commit de las 3

--- Lab3P1_LeonardoLopez/src/lab3p1_leonardolopez/Lab3P1_LeonardoLopez.java
+++ Lab3P1_LeonardoLopez/src/lab3p1_leonardolopez/Lab3P1_LeonardoLopez.java
@@ -4,6 +4,8 @@
  */
 package lab3p1_leonardolopez;
 
+import java.util.Scanner;
+
 /**
  *
  * @author leona
@@ -14,7 +16,67 @@ public class Lab3P1_LeonardoLopez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner leer = new Scanner(System.in);
+       int opcion = 0;
+           System.out.println("-----Menu-----");
+           System.out.println("0. Salir");
+           System.out.println("1. Division sin '/'");
+           System.out.println("2. Piedra, papel, o tijeras");
+           System.out.println("3. Zigzag");
+           System.out.print("Escoja: ");
+           opcion = leer.nextInt();
+       while (opcion != 0){
+           
+           
+           while (opcion < 0 || opcion >= 4){
+            System.out.print(" ingrese la opcion correcta: ");
+            opcion= leer.nextInt();
+           } 
+            
+           switch (opcion){
+               case 1:{
+                   int dividendo = 0;
+                    int divisor = 0;
+
+        
+                while (dividendo <= 1) {
+                    System.out.print("Ingrese un numero dividendo mayor a 1: ");
+                    dividendo = leer.nextInt();
+                if (dividendo <= 1) {
+                    System.out.println("Número invalido. Intente de nuevo.");
+                }
+            }
+
+                while (divisor <= 1) {
+                    System.out.print("Ingrese un numero divisor mayor a 1: ");
+                    divisor = leer.nextInt();
+                if (divisor <= 1) {
+                    System.out.println("Número invalido. Intente de nuevo.");
+                }
+                }
+
+    
+                int cociente = 0;
+                int residuo = dividendo;
+
+                while (residuo >= divisor) {
+                    residuo -= divisor;
+                    cociente++;
+                }
+
+                System.out.println("El resultado de la division es: " + cociente);
+                System.out.println("El residuo es: " + residuo);
+           }
+            case 2:{
+         
+                   
+                    
+                     
+            }
+           
+       }
+       
     }
     
+    }
 }
